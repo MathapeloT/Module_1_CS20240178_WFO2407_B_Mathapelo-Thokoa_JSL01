13cf307787f8db8718b4caa76b8d78c60bf6ab6a
MathapeloT <thokoamathapelo3@gmail.com>
2024-08-31 14:28:17 +0200

    Adds code to provide message for input entered in correct format

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+  const pattern = 'pet_' + 'Year' + 'petName'
+    let result = 'Valid Syntax';
+
+
+   if (validateSyntax(petInput = pattern)) {
+    console.log(result);
+   } else {
+    console.log('Invalid Syntax!');
+   }
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
-            document.getElementById('result').innerText = result;
+    document.getElementById('result').innerText = result;    
 }
-
-
