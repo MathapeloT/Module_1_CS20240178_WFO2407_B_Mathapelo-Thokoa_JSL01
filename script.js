110e8264b5b343a1fc53f348b868ce97dbebb256
MathapeloT <thokoamathapelo3@gmail.com>
2024-09-16 03:07:36 +0200

    Modifies code to create more fuuntional output results

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
-  const pattern = 'pet_' + 'Year' + 'petName'
-    let result = 'Valid Syntax';
-
-
-   if (validateSyntax(petInput = pattern)) {
-    console.log(result);
-   } else {
-    console.log('Invalid Syntax!');
-   }
+    let result = "";
+    const regexp = /^pet_\d{4}[^\d_]+$/;
+    const resultElement = document.getElementById("result");
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-
+    result = regexp.test(input) ? "Valid Syntax🐱" : "Invalid Syntax!🙄";
     document.getElementById('result').innerText = result;    
 }
